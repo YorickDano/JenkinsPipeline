bc4f706f7a90fed989b64fdeac5ed19a6071dbd0
Nikita Levdanskii <nikita_levdanskii@epam.com>
2022-11-12 18:07:07 +0300

    Add project files.

---
+++ JenkinsPipeline/ExecuteManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsPipeline
+{
+    public static class ExecuteManager
+    {
+        public static int GetSummaryOfArray(int[] numbers) => numbers.Sum();
+
+        public static int[] GetArrayOfOddIndexNumbers(int[] numbers) => numbers.Where(x => Array.IndexOf(numbers, x) % 2 != 0 ).ToArray();
+
+        public static int[] GetArrayOfEvenIndexNumbers(int[] numbers) => numbers.Where(x => Array.IndexOf(numbers, x) % 2 == 0 && Array.IndexOf(numbers, x) != 0).ToArray();
+
+    }
+}
---
+++ Tests/SmokeTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsPipeline
+{
+    [TestFixture]
+    public class SmokeTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void SumOfArrayTest()
+        {
+            var testDataNumbers = new int[] { 1, 2, 3 };
+            var expectedData = 6;
+            var actualData = ExecuteManager.GetSummaryOfArray(testDataNumbers);
+
+            Assert.That(actualData, Is.EqualTo(expectedData));
+        }
+
+        [Test]
+        public void OddIndexNumbersOfArrayTest()
+        {
+            var testDataNumbers = new int[] { 1, 2, 3, 4, 5 };
+            var expectedData = new int[] { 2, 4};
+            var actualData = ExecuteManager.GetArrayOfOddIndexNumbers(testDataNumbers);
+
+            Assert.That(actualData, Is.EqualTo(expectedData));
+        }
+
+        [Test]
+        public void EvenIndexNumbersOfArrayTest()
+        {
+            var testDataNumbers = new int[] { 1, 2, 3, 4, 5 };
+            var expectedData = new int[] { 3, 5 };
+            var actualData = ExecuteManager.GetArrayOfEvenIndexNumbers(testDataNumbers);
+
+            Assert.That(actualData, Is.EqualTo(expectedData));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
